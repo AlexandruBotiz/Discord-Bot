79c698fdf4ab69dbec43c0b4e755b520df2bb5d8
raul-it0o0 <ariton.raul@gmail.com>
2025-08-12 11:22:22 +0300

    Reformat files

--- src/slash-deploy.js
+++ src/slash-deploy.js
@@ -1,27 +1,28 @@
-import {REST , Routes } from "discord.js";
-import dotenv from "dotenv";
+import { REST, Routes } from 'discord.js';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 //info needed for slash commands
-const botID = "1401818514372825230"
-const serverID = "1401820681473364078"
-const botToken = process.env.DISCORD_TOKEN
+const botID = '1401818514372825230';
+const serverID = '1401820681473364078';
+const botToken = process.env.DISCORD_TOKEN;
 
-const rest = new REST({ version: '10' }).setToken(botToken)
+const rest = new REST({ version: '10' }).setToken(botToken);
 
 const slashRegister = async () => {
-    try {
-        await rest.put(Routes.applicationGuildCommands(botID, serverID), {
-            body: [
-                {
-                    name: "brainbuzz",
-                    description: "Test message"
-                }
-            ]
-        })
-    } catch (error) {
-        console.error(error)
-    }
-}
+  try {
+    await rest.put(Routes.applicationGuildCommands(botID, serverID), {
+      body: [
+        {
+          name: 'brainbuzz',
+          description: 'Test message'
+        }
+      ]
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 slashRegister();
